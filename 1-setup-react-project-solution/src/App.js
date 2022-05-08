d3e21acbfeeff77577055d84b0265f3bf9dbe891
V M Aravind <vmaravind07@gmail.com>
2022-05-08 12:29:51 +0530

    completed second task

--- 1-setup-react-project-solution/src/App.js
+++ 1-setup-react-project-solution/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import styled from "styled-components";
+
+const Header = styled.h1`
+  color: red;
+  @media (max-width: 480px) {
+    font-size: 24px;
+    color: green;
+  }
+`;
+const Hero = styled(Header)`
+  color: blue;
+  font-size: 100px;
+`;
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Hero>Iam a Hero</Hero>
+      <Header>Hello React!</Header>;
+    </div>
+  );
 }
 
 export default App;
